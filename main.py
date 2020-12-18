6a463f3d89bd9c2a2423813602244ed93f5f539f
FeiyiWang <rephycca@gmail.com>
2020-12-18 08:34:29 -0500

    use disease pair to calculate

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import openai
-import uuid, re
+import pandas as pd
+import numpy as np
+import lifelines
+import datetime, re
 from fuzzywuzzy import process
 from gpt import GPT, Example
 from flask import Flask, request, jsonify
 import json
+import itertools
 
 app = Flask(__name__, static_url_path='')
 
-# global noteData
-# global cancerData
-# global ptData
-# session_dict = {'is_data_processed': False}
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://gpt3:YQkwRnR!@X&FVV%&5)Xs5QnYezh*XH5g2Ej432q8w*w&weeFioNxksY8guAd@vDg@localhost:5432/risteys_blue_r6')
+
 global gpt
 
-openai.api_key = "sk-MKxaEJsHbFyW5U7bP2e0d2B9mzpoAxuWQ73sDmo2"
+# openai.api_key = "sk-MKxaEJsHbFyW5U7bP2e0d2B9mzpoAxuWQ73sDmo2" #old
+openai.api_key = "sk-BOwN8oP1QsmOe2SaXqnuy7U65UkPm1f72xGAYsRm" #new
 
 gpt = GPT(engine="davinci",
           temperature=0,
-          max_tokens=300,
-          input_prefix="Q: ",
-          input_suffix="(q_end)",
-          output_prefix="A: ",
-          output_suffix="(a_end)",
-          append_output_prefix_to_query=True)
+          max_tokens=300)
 
 
 gpt.add_example(
@@ -35,11 +34,14 @@ gpt.add_example(
     Example('I am at 40 with angina. What is my risk of having headache?',
             'angina, headache, 40, na'))
 gpt.add_example(
+    Example('I am a guy at 47 with migraine. What is my risk of stroke?',
+            'migraine, stroke, 47, male'))
+gpt.add_example(
     Example('What is my risk of angina if I am a female with a history of heart attack?',
             'heart attack, angina, na, female'))
 gpt.add_example(
     Example('I am a girl at 13 with migraine?',
-            'migraine, na, 13, female'))
+            'migraine, na, 13, 1'))
 gpt.add_example(
     Example('If I have diabetes earlier, what is my risk of having hypertension?',
             'diabetes, hypertension, na, na'))
@@ -54,7 +56,9 @@ gpt.add_example(
 # I am a 65-year-old male. I had strke. What is my risk of epilepsy?
 # I am a white American guy at 47 with migraine. What is my risk of stroke?
 # I am a girl at 27 with lung cancer. I come from Japan. What is my risk of anemia?
-
+# What is my risk of having heart failure if I had cardiovascular disease earlier? I am a woman at 65.
+# Suppose I have cardiovascular disease and I am a man at 70. What's my risk of having heart failure problem?
+# I am guy with strke. What is my risk of epilepsy?
 
 # If I am a girl at 24 with lung cancer, what's my risk of having heart disease?
 # I am a 65-year-old male. I had strke. What is my risk of epilepsy?
@@ -63,49 +67,110 @@ gpt.add_example(
 # I am a girl at 27 with lung cancer. I come from Japan. What is my risk of anemia?
 # I am a 65-year-old white American male. I had stroke. What is my risk of epilepsy?
 
-# diag_df = pd.read_excel('ENDPOINT_ICD_FIN.xlsx', sheet_name='FINAL')
-# diag_list = list(diag_df.LONGNAME)
-with open('endpoint_list.json') as f:
-    diag_list = json.load(f)
-# diag_list = [i.lower() for i in diag_list]
 
-def processAnswer(answer,match_part):
+with open('endpoint_list.json') as f:
+    endpoint_list = json.load(f)
+endpoint_list = [(i[0],i[1]) for i in endpoint_list]
+prior_list = set(list(zip(*endpoint_list))[0])
+outcome_list = set(list(zip(*endpoint_list))[1])
+
+def processAnswer(answer):
     '''
-    match_part - 'disease_pre', 'disease_post'
+    Input:
+    answer pattern: list - 'disease_pre', 'disease_post', 'age', 'sex' (M: 0; F: 1)
+    e.g. ['cancer', 'anemia', '27', '0']
+
+    Output:
+    question_index: dictionary -  pair: 1 - multi disease pairs are found; 2 - No disease pair is found
+                                  age: 1 - no age
+                                  sex: 1 - no gender
+    question_dict: dictionary -  {pair:'', age:'', sex:''}
+    choice_dict: dictionary -  {pre:'', post:''}
+    modified_answer
     '''
-    if match_part == 'prior':
-        index = 0
-        a = 'disease'
-    else:
-        index = 1
-        a = 'risk'
+    #TODO: create a def for disease pre and post to avoid cohesion
+    #TODO: what to do if no disease is found?
+    #TODO: what if len of answer is not 4?
+
+    question_dict = {'pair':'Can you specify the disease you have and the risk you concern?',
+                     'age':'Can you tell me your age?',
+                     'sex':'Can you tell me your gender?'}
+    choice_dict, question_index = {}, {}
+
+    if answer[0] == 'na' or answer[1] == 'na':
+        question_dict['pair'] = 'Sorry'
+        question_index['pair'] = 2
+        return answer, question_index, question_dict, choice_dict
+
+    if answer[-1] == 'female':
+        answer[-1] = '1'
+    elif answer[-1] == 'male':
+        answer[-1] = '0'
+
     try:
-        disease_pre = answer[index]
-        disease = re.sub(r'(problem|disease)s{0,}( in){0,1}','',disease_pre).strip()
-        disease_list = [diag for diag in diag_list if disease.lower().capitalize() in diag]
-    except:
-        print(answer)
-        disease_list = []
-    if len(disease_list) == 0:
-        ratio = process.extract(disease.lower().capitalize(),diag_list)
-        disease_list = [i[0] for i in ratio]
-#     try:
-#         disease_pre = re.search(r'disease_pre = "([a-z][a-z\s]*)"', answer).group(1)
-#         disease_pre_list = [diag for diag in diag_list if disease_pre.upper() in diag]
-#     except:
-#         # cannot find anything
-#         return 
-    if len(disease_list) > 1:
-        if match_part == 'outcome':
-            q = 'Hey, what type of '+a+' do you have?'
+
+        disease_pre = re.sub(r'(problem|disease)s{0,}( in){0,1}','',answer[0])
+        disease_post = re.sub(r'(problem|disease)s{0,}( in){0,1}','',answer[1])
+        disease_list_pre = [diag for diag in prior_list if disease_pre.strip() in diag.lower()]
+        disease_list_post = [diag for diag in outcome_list if disease_post.strip() in diag.lower()]
+        disease_pair = list(itertools.product(disease_list_pre, disease_list_post))
+        possible_disease_pair = [i for i in disease_pair if i in endpoint_list]
+        print(disease_pre)
+        print()
+        print(possible_disease_pair)
+
+        if len(possible_disease_pair) == 0:
+            ratio_pre = process.extract(disease_pre.lower().capitalize(),prior_list)
+            disease_list_pre = [i[0] for i in ratio_pre]
+            ratio_post = process.extract(disease_pre.lower().capitalize(),outcome_list)
+            disease_list_post = [i[0] for i in ratio_post]
+            disease_pair = list(itertools.product(disease_list_pre, disease_list_post))
+            possible_disease_pair = [i for i in disease_pair if i in endpoint_list]
+
+        if len(possible_disease_pair) > 1:
+            question_index['pair'] = 1
+            choice_dict['pre'] = list(set(list(zip(*possible_disease_pair))[0]))
+            choice_dict['post'] = list(set(list(zip(*possible_disease_pair))[1]))
+        elif len(possible_disease_pair) == 0:
+            question_dict['pre'] = 'No disease in the database matches the one you have, can you describe it in another way?'
+            question_index['pair'] = 2
+            return answer, question_index, question_dict, choice_dict
         else:
-            q = 'Hey, what type of '+a+' you are asking?'
-#         return 'Hey, what type of '+disease_pre+' do you have?\n'+str(disease_list)
-    elif len(disease_list) == 0:
-        q = 'No disease in db matches the '+a+' you described, can you say it in another way?'
-    else:
-        q = disease
-    return q, disease_list
+            question_index['pair'] = 0
+            answer[0], answer[1] = possible_disease_pair[0][0], possible_disease_pair[0][1]
+
+        # if len(disease_list_post) == 0:
+        #     ratio_post = process.extract(disease_post.lower().capitalize(),outcome_list)
+        #     disease_list_post = [i[0] for i in ratio_post]
+        # if len(disease_list_post) > 1:
+        #     question_index['post'] = 1
+        #     choice_dict['post'] = disease_list_post
+        # elif len(disease_list_post) == 0:
+        #     question_dict['post'] = 'No disease in the database matches the risk you concern, can you describe it in another way?'
+        #     question_index['post'] = 2
+        # else:
+        #     question_index['post'] = 0
+        #     answer[1] = disease_list_post[0]
+
+        if answer[2] == 'na':
+            question_index['age'] = 1
+        if answer[3] == 'na':
+            question_index['sex'] = 1
+
+        if answer[0] == 'na':
+            question_dict['pre'] = 'No disease in the database matches the one you have, can you describe it in another way?'
+            question_index['pre'] = 2
+        if answer[1] == 'na':
+            question_dict['post'] = 'No disease in the database matches the risk you concern, can you describe it in another way?'
+            question_index['post'] = 2
+
+        answer = [possible_disease_pair[0][0], possible_disease_pair[0][1],answer[2],answer[3]]
+
+    except Exception as e:
+        print(e)
+        print(answer)
+
+    return answer, question_index, question_dict, choice_dict
 
 @app.route('/')
 
@@ -123,21 +188,40 @@ def translate():
     #     return response['choices'][0]['text'][offset:-len(gpt.output_suffix)]
     # pylint: disable=unused-variable
     prompt = request.json["prompt"]
-    answer = gpt.submit_request(prompt)['choices'][0]['text']
-    text_pre, list_pre = processAnswer(answer.split(', '),'prior')
-    text_post, list_post = processAnswer(answer.split(', '),'outcome')
+    answer = gpt.submit_request(prompt)['choices'][0]['text'].split(', ')
+    print(answer)
+    answer, question_index, question_dict, choice_dict = processAnswer(answer)
 
-    # return jsonify(answer: response,text_pre: text_pre,text_post: text_post,list_pre: list_pre,list_post:list_post)
     msg = {
         'answer': answer,
-        'text_pre': text_pre,
-        'text_post': text_post,
-        'list_pre': list_pre,
-        'list_post': list_post
+        'question_index': question_index,
+        'question_dict': question_dict,
+        'choice_dict': choice_dict
     }
-    # print(msg['text_pre'])
     print(msg['answer'])
+    print(msg['question_index'])
+
     return jsonify(message = msg)
+
+@app.route("/getRisk", methods=["POST"])
+def getRisk():
+    result = request.json["result"].split(',')
+    prior = result[0].replace('&',',')
+    outcome = result[1].replace('&',',')
+    follow_up_years = request.json["years"]
+    mean_indiv = pd.DataFrame({
+        "BIRTH_TYEAR": [datetime.datetime.today().year - int(result[2])],
+        "endpoint": [True],
+        "female": [int(result[3])]
+    })
+    try:
+        r = pd.read_sql_query("SELECT * FROM cox_hrs as c, phenocodes as p_a, phenocodes as p_b WHERE p_a.id = c.prior_id AND p_b.id = c.outcome_id AND c.lagged_hr_cut_year = "+follow_up_years+" AND p_a.longname =  '" + prior + "' AND p_b.longname = '" + outcome + "';", engine).iloc[0,:]
+        abs_risk = 1 - np.exp(- r.bch_year_21p99 * np.exp( np.dot(lifelines.utils.normalize(mean_indiv, mean=[r.year_norm_mean, r.prior_norm_mean, r.sex_norm_mean], std=1), [r.year_coef, r.prior_coef, r.sex_coef])))[0]
+        return jsonify(message = {"part1":"Your risk of having " + outcome + " is ", "part2":"{0:.2%}".format(abs_risk)})
+    except IndexError as e1:
+        print('Error type:'+str(e1))
+        return jsonify(message = {"part1":"No record is found with the input: \n", "part2":str([prior, outcome, datetime.datetime.today().year - int(result[2]), result[3]])})
+
 
 if __name__ == '__main__':
 
